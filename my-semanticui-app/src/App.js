e0706bdb15c630fa57d5ca6a6294f74fb1ff4220
Nazeer <nazeersd.java@gmail.com>
2022-10-11 01:35:52 +0530

    updated

--- my-semanticui-app/src/App.js
+++ my-semanticui-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import { Progress, Button } from 'semantic-ui-react'
 
 function App() {
+  const [progress, setProgress] = useState(0)
+  const fnIncrease = () => {
+    setProgress(progress + 10)
+  }
+  const fnDecrease = () => {
+    setProgress(progress - 10)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='App-header'>Welcome to Semantic UI React</div>
+      <marquee>Semantic UI React provides React components while Semantic UI provides themes as CSS stylesheets. Install the React components and choose a theme that suits your needs.</marquee>
+      <h2 className='App-center'> Semantic UI is a user-centric React UI framework that simplifies HTML into an easily readable form.</h2>
+      <Progress percent={progress} indicating />
+      <div className='App-center'>
+        <Button onClick={fnIncrease}>Increment</Button>
+        <Button onClick={fnDecrease}>Decrement</Button>
+        <Button onClick={() => { setProgress(0) }}>Reset</Button>
+      </div>
+      <div className='App-footer'>&copy; Blazing deployments by Vercel.Inc</div>
     </div>
   );
 }
